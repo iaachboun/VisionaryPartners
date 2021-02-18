63bbd5a19470e7836f4bbcc218aea7ef22079e6e
ChenpeiHu <chenpeihu6@gmail.com>
2021-02-18 22:54:20 +0100

    header fixed, new logo images

--- header.php
+++ header.php
@@ -23,27 +23,35 @@
     <div class="main_menu">
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <a class="navbar-brand logo_h" href="index.php"><img src="img/logo/siteLogo_white.svg" alt="" class="siteLogo"></a>
+                <div class="row header-row">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <a class="navbar-brand mr-0 logo_h col-11 col-lg-4 col-xl-5 d-flex justify-content-center align-items-center" href="index.php">
+                        <img src="img/logo/SiteLogoShort_white.png" alt="Logo small" class="siteLogoSmall d-lg-none">
+                        <img src="img/logo/siteLogo_white.svg" alt="Logo large" class="siteLogo d-none d-lg-block">
+                    </a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-                    <ul class="nav navbar-nav menu_nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="feature.php">Features</a></li>
-                        <li class="nav-item"><a class="nav-link" href="price.php">Price</a></li>
-                        <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-                    </ul>
-                </div>
-                <div class="right-button">
-                    <ul>
-                        <li><a class="sign_up" href="contact.php">Begin nu</a></li>
-                    </ul>
+                    <button class="navbar-toggler col-1" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+
+                    <div class="collapse navbar-collapse offset col-11 col-lg-5 mx-auto" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav menu_nav mx-auto">
+                            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="feature.php">Features</a></li>
+                            <li class="nav-item"><a class="nav-link" href="price.php">Price</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="right-button col-3 col-xl-2">
+                        <a class="sign_up" href="contact.php">Begin nu</a>
+                    </div>
                 </div>
             </div>
         </nav>
