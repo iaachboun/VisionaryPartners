4afa7bc16abc052ce0d8dbbed69742be364e70f3
germain <menus2011@hotmail.com>
2021-05-31 02:25:44 +0200

    #011 - copyright tekst toegevoegd

--- index.php
+++ index.php
@@ -224,11 +224,9 @@ require_once __DIR__ . '/header.php'
             </div>
         </div>
         <div class="footer-bottom row align-items-center text-center text-lg-left no-gutters">
-            <p class="footer-text m-0 col-lg-8 col-md-12">
+            <p class="footer-text m-0 col-lg-8 col-md-12 center">
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;<script>document.write(new Date().getFullYear());</script>
-                All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a
-                        href="https://colorlib.com" target="_blank">Colorlib</a>
+                Copyright &copy;<script>document.write(new Date().getFullYear());</script><a href="https://colorlib.com" target="_blank"> VisionaryPartners</a>
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
             <div class="col-lg-4 col-md-12 text-center text-lg-right footer-social">
                 <a href="#"><i class="ti-facebook"></i></a>
