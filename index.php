f4a153ef7082a1ba42528620d71d9f97b24064bb
germain <menus2011@hotmail.com>
2021-05-31 02:11:32 +0200

    #001 - Geoptimaliseerd voor zoekmachines verwijderd

--- index.php
+++ index.php
@@ -161,45 +161,6 @@ require_once __DIR__ . '/header.php'
 </section>
 <!--================Feature Area End =================-->
 
-
-<!--================About  Area =================-->
-<section class="statics-area area-padding">
-    <div class="container">
-        <div class="row">
-
-            <div class="col-lg-5">
-                <div class="image-box">
-                    <img src="img/banner/about3.png" alt="">
-                </div>
-            </div>
-
-            <div class="offset-lg-1 col-lg-6">
-                <div class="area-heading">
-                    <h4>Geoptimaliseerd voor zoekmachines</h4>
-                    <h6>hello world</h6>
-
-                    <p>There earth face earth behold. She'd stars made void two given do and also. Our own grass days.
-                        Greater male Shall There faced earth behold She star</p>
-                </div>
-                <div class="single-data">
-                    <i class="ti-paint-bucket"></i>
-                    <p>Set dry signs spirit a kind First shall them.</p>
-                </div>
-                <div class="single-data">
-                    <i class="ti-check-box"></i>
-                    <p>He two face one moved dominion man you're likeness</p>
-                </div>
-                <div class="single-data">
-                    <i class="ti-ruler-pencil"></i>
-                    <p>Sea forth fill have divide be dominion from life</p>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</section>
-<!--================About Area End =================-->
-
 <!--================ Start Brands Area =================-->
 <section class="brands-area area-padding-bottom">
     <div class="container">
