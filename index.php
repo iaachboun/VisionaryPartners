045561c1dd2c2851ede1f026b9cf1ee15f7cac56
germain <menus2011@hotmail.com>
2021-05-31 02:12:24 +0200

    #002 - brand logo's verwijderd

--- index.php
+++ index.php
@@ -161,56 +161,6 @@ require_once __DIR__ . '/header.php'
 </section>
 <!--================Feature Area End =================-->
 
-<!--================ Start Brands Area =================-->
-<section class="brands-area area-padding-bottom">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-10">
-                <div class="owl-carousel brand-carousel">
-                    <!-- single-brand -->
-                    <div class="single-brand-item d-table">
-                        <div class="d-table-cell">
-                            <img src="img/logo/1.png" alt="">
-                        </div>
-                    </div>
-                    <!-- single-brand -->
-                    <div class="single-brand-item d-table">
-                        <div class="d-table-cell">
-                            <img src="img/logo/2.png" alt="">
-                        </div>
-                    </div>
-                    <!-- single-brand -->
-                    <div class="single-brand-item d-table">
-                        <div class="d-table-cell">
-                            <img src="img/logo/3.png" alt="">
-                        </div>
-                    </div>
-                    <!-- single-brand -->
-                    <div class="single-brand-item d-table">
-                        <div class="d-table-cell">
-                            <img src="img/logo/4.png" alt="">
-                        </div>
-                    </div>
-                    <!-- single-brand -->
-                    <div class="single-brand-item d-table">
-                        <div class="d-table-cell">
-                            <img src="img/logo/5.png" alt="">
-                        </div>
-                    </div>
-                    <!-- single-brand -->
-                    <div class="single-brand-item d-table">
-                        <div class="d-table-cell">
-                            <img src="img/logo/3.png" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!--================ End Brands Area =================-->
-
-
 <!-- ================ start footer Area ================= -->
 <footer class="footer-area">
     <div class="container">
