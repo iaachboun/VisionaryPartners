df91ebf282a067c90da35e12a4b4b72f77e6f9fe
Ilias <ilias@aravis.nl>
2023-10-23 14:02:27 +0200

    mobile res

--- src/components/Pakketten.vue
+++ src/components/Pakketten.vue
@@ -4,7 +4,7 @@
   </div>
 
   <div class="grid grid-cols-1 flex items-center lg:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4 ml-auto">
-    <div class="bg-black rounded-3xl text-left w-72 m-auto mb-10">
+    <div class="bg-black rounded-3xl text-left w-72 m-auto mb-10 h-[546px] cursor-default">
       <p class="pl-7 py-5 font-semibold text-2xl">E-Commerce</p>
       <hr class="h-px border-0 bg-[#808080]">
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>CMS Systeem</p>
@@ -17,7 +17,7 @@
       <p class="mx-6 mt-6 text-[#808080]">Vanaf <strong class="text-white float-right pr-2">€499 - €1499</strong></p>
       <button class="p-2 bg-[#D6FF01] text-black font-semibold px-20 mx-6 my-6 rounded-xl">START NU</button>
     </div>
-    <div class="bg-black rounded-3xl text-left w-72 m-auto mb-10">
+    <div class="bg-black rounded-3xl text-left w-72 m-auto mb-10 h-[546px] cursor-default">
       <p class="pl-7 py-5 font-semibold text-2xl">Website</p>
       <hr class="h-px border-0 bg-[#808080]">
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>Zakelijke Mail</p>
@@ -25,11 +25,13 @@
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>Social Media Integratie</p>
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>Google Geoptimaliseerd</p>
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>24/7 Support</p>
+      <p class="pl-7 py-4 text-[#808080] select-none text-black">---</p>
+
       <hr class="h-px border-0 bg-[#808080]">
       <p class="mx-6 mt-6 text-[#808080]">Vanaf <strong class="text-white float-right pr-2">€299</strong></p>
       <button class="p-2 bg-[#D6FF01] text-black font-semibold px-20 mx-6 my-6 rounded-xl">START NU</button>
     </div>
-    <div class="bg-black rounded-3xl text-left w-72 m-auto mb-10">
+    <div class="bg-black rounded-3xl text-left w-72 m-auto mb-10 h-[546px] cursor-default">
       <p class="pl-7 py-5 font-semibold text-2xl">App</p>
       <hr class="h-px border-0 bg-[#808080]">
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>Maatwerk</p>
@@ -37,11 +39,12 @@
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>Optimale Snelheid</p>
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>Playstore/Appstore</p>
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>24/7 Support</p>
+      <p class="pl-7 py-4 text-[#808080] select-none text-black">---</p>
       <hr class="h-px border-0 bg-[#808080]">
       <p class="mx-6 mt-6 text-[#808080]">Vanaf <strong class="text-white float-right pr-2">€599</strong></p>
       <button class="p-2 bg-[#D6FF01] text-black font-semibold px-20 mx-6 my-6 rounded-xl">START NU</button>
     </div>
-    <div class="bg-black rounded-3xl text-left w-72 m-auto mb-10">
+    <div class="bg-black rounded-3xl text-left w-72 m-auto mb-10 h-[546px] cursor-default">
       <p class="pl-7 py-5 font-semibold text-2xl">Hosting/ Onderhoud</p>
       <hr class="h-px border-0 bg-[#808080]">
       <p class="pl-7 py-4 text-[#808080]"><i class="fa-solid fa-circle-check mr-4 text-white"></i>99.9% Uptime</p>
--- src/components/Specificaties.vue
+++ src/components/Specificaties.vue
@@ -1,85 +1,87 @@
 <template>
-  <div class="container mx-auto sm:w-8/12">
+
+  <div class="container mx-auto w-9/12">
     <div class="grid grid-cols-1 sm:grid-cols-2">
-      <div>
-        <div class="text-left grid grid-cols-12 my-5 max-w-[425px] mx-auto p-5">
-          <div class="float-left text-2xl col-span-2">
-            01
-          </div>
-          <div class="col-span-10 max-w-[425px]">
-            <h3 class="text-2xl mb-2">CMS Systeem</h3>
-            <p class="font-light">Met ons gebruiksvriendelijke Content Management Systeem (CMS) kun je <strong
-                class="text-[#D6FF01]">op elk moment zelf
-              teksten aanpassen.</strong>
-              Dit betekent dat je de <strong class="text-[#D6FF01]">volledige controle </strong> hebt over de inhoud
-              van
-              je
-              website, waardoor het <strong class="text-[#D6FF01]">snel en eenvoudig</strong>
-              is om je site een nieuwe stijl te geven.</p>
-          </div>
+      <div class="text-left grid grid-cols-12 mb-5 mx-auto sm:mx-0">
+        <div class="float-left text-4xl col-span-2">
+          01
         </div>
-        <div class="text-left grid grid-cols-12 my-5 max-w-[425px] mx-auto p-5">
-          <div class="float-left text-2xl col-span-2">
-            02
-          </div>
-          <div class="col-span-10 max-w-[425px]">
-            <h3 class="text-2xl mb-2">SEO Optimalisatie</h3>
-            <p class="font-light">Onze SEO-experts zorgen ervoor dat jouw website <strong
-                class="text-[#D6FF01]">hoog in de zoekresultaten van Google verschijnt.</strong>
-              Dankzij zoekmachineoptimalisatie (SEO) vergroten we de zichtbaarheid van je website en trekken we meer
-              potentiële klanten aan.</p>
-          </div>
-        </div>
-        <div class="text-left grid grid-cols-12 my-5 max-w-[425px] mx-auto p-5">
-          <div class="float-left text-2xl col-span-2">
-            03
-          </div>
-          <div class="col-span-10 max-w-[425px]">
-            <h3 class="text-2xl mb-2">Website Onderhoud</h3>
-            <p class="font-light">Met Visionary Partners kun je jouw <strong
-                class="text-[#D6FF01]">website onderhouden </strong>
-              in onze <strong class="text-[#D6FF01]">beveiligde cloudomgeving.</strong> Wij garanderen een uptime van
-              99,9% en zorgen ervoor dat je site altijd up-to-date is met de nieuwste versies en beveiligingspatches.
-            </p>
-          </div>
+        <div class="col-span-10 sm:w-[425px]">
+          <h3 class="text-4xl mb-2">CMS Systeem</h3>
+          <p class="font-light text-lg">Met ons gebruiksvriendelijke Content Management Systeem (CMS) kun je <strong
+              class="text-[#D6FF01]">op elk moment zelf
+            teksten aanpassen.</strong>
+            Dit betekent dat je de <strong class="text-[#D6FF01]">volledige controle </strong> hebt over de inhoud
+            van
+            je
+            website, waardoor het <strong class="text-[#D6FF01]">snel en eenvoudig</strong>
+            is om je site een nieuwe stijl te geven.</p>
         </div>
       </div>
-
-      <div>
-        <div class="text-left grid grid-cols-12 my-5 max-w-[425px] mx-auto p-5">
-          <div class="float-left text-2xl col-span-2">
-            04
-          </div>
-          <div class="col-span-10 max-w-[425px]">
-            <h3 class="text-2xl mb-2">Maatwerk</h3>
-            <p class="font-light">Jouw bedrijf is uniek, en jouw website moet dat ook zijn. Wij nemen maatwerk serieus
-              en <strong
-                  class="text-[#D6FF01]">streven ernaar om samen met jou een uniek online product te creëren </strong>
-              dat de essentie van jouw bedrijf weerspiegelt.</p>
-          </div>
+      <div class="text-left grid grid-cols-12 mb-5 sm:ml-10 mx-auto sm:mx-0">
+        <div class="float-left text-4xl col-span-2">
+          04
         </div>
-        <div class="text-left grid grid-cols-12 my-5 max-w-[425px] mx-auto p-5">
-          <div class="float-left text-2xl col-span-2">
-            05
-          </div>
-          <div class="col-span-10 max-w-[425px]">
-            <h3 class="text-2xl mb-2">Gezamenlijke Ideeën</h3>
-            <p class="font-light">Onze ervaren teamleden begeleiden je bij het verkennen van unieke en creatieve ideeën
-              voor je website of app. We luisteren naar al je eisen en wensen en combineren deze met onze professionele
-              inzichten voor een <strong class="text-[#D6FF01]">succesvol eindresultaat</strong></p>
-          </div>
+        <div class="col-span-10 sm:w-[425px]">
+          <h3 class="text-4xl mb-2">Maatwerk</h3>
+          <p class="font-light text-lg">Jouw bedrijf is uniek, en jouw website moet dat ook zijn. Wij nemen maatwerk
+            serieus
+            en <strong
+                class="text-[#D6FF01]">streven ernaar om samen met jou een uniek online product te creëren </strong>
+            dat de essentie van jouw bedrijf weerspiegelt.</p>
         </div>
-        <div class="text-left grid grid-cols-12 my-5 max-w-[425px] mx-auto p-5">
-          <div class="float-left text-2xl col-span-2">
-            06
-          </div>
-          <div class="col-span-10 max-w-[425px]">
-            <h3 class="text-2xl mb-2">punctuele Oplevering</h3>
-            <p class="font-light">We beloven dat jouw website op tijd wordt opgeleverd, zoals afgesproken. <strong
-                class="text-[#D6FF01]">We regelen alles </strong>
-              van tevoren, inclusief certificaten, domeinnamen en hosting, zodat je geen tijd hoeft te verspillen aan
-              administratieve zaken.</p>
-          </div>
+      </div>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2">
+      <div class="text-left grid grid-cols-12 mb-5 mx-auto sm:mx-0">
+        <div class="float-left text-4xl col-span-2">
+          02
+        </div>
+        <div class="col-span-10 sm:w-[425px]">
+          <h3 class="text-4xl mb-2">SEO Optimalisatie</h3>
+          <p class="font-light text-lg">Onze SEO-experts zorgen ervoor dat jouw website <strong
+              class="text-[#D6FF01]">hoog in de zoekresultaten van Google verschijnt.</strong>
+            Dankzij zoekmachineoptimalisatie (SEO) vergroten we de zichtbaarheid van je website en trekken we meer
+            potentiële klanten aan.</p>
+        </div>
+      </div>
+      <div class="text-left grid grid-cols-12 mb-5 sm:ml-10 mx-auto sm:mx-0">
+        <div class="float-left text-4xl col-span-2">
+          05
+        </div>
+        <div class="col-span-10 sm:w-[425px]">
+          <h3 class="text-4xl mb-2">Gezamenlijke Ideeën</h3>
+          <p class="font-light text-lg">Onze ervaren teamleden begeleiden je bij het verkennen van unieke en creatieve
+            ideeën
+            voor je website of app. We luisteren naar al je eisen en wensen en combineren deze met onze professionele
+            inzichten voor een <strong class="text-[#D6FF01]">succesvol eindresultaat</strong></p>
+        </div>
+      </div>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2">
+      <div class="text-left grid grid-cols-12 mb-5 mx-auto sm:mx-0">
+        <div class="float-left text-4xl col-span-2">
+          03
+        </div>
+        <div class="col-span-10 sm:w-[425px]">
+          <h3 class="text-4xl mb-2">Website Onderhoud</h3>
+          <p class="font-light text-lg">Met Visionary Partners kun je jouw <strong
+              class="text-[#D6FF01]">website onderhouden </strong>
+            in onze <strong class="text-[#D6FF01]">beveiligde cloudomgeving.</strong> Wij garanderen een uptime van
+            99,9% en zorgen ervoor dat je site altijd up-to-date is met de nieuwste versies en beveiligingspatches.
+          </p>
+        </div>
+      </div>
+      <div class="text-left grid grid-cols-12 mb-5 sm:ml-10 mx-auto sm:mx-0">
+        <div class="float-left text-4xl col-span-2">
+          06
+        </div>
+        <div class="col-span-10 sm:w-[425px]">
+          <h3 class="text-4xl mb-2">punctuele Oplevering</h3>
+          <p class="font-light text-lg">We beloven dat jouw website op tijd wordt opgeleverd, zoals afgesproken. <strong
+              class="text-[#D6FF01]">We regelen alles </strong>
+            van tevoren, inclusief certificaten, domeinnamen en hosting, zodat je geen tijd hoeft te verspillen aan
+            administratieve zaken.</p>
         </div>
       </div>
     </div>
