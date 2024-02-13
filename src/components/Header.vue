7f8680910afb38cf1112bcaa12d6e72f2071ac3e
Ilias <ilias@aravis.nl>
2024-02-13 10:58:21 +0100

    menu to cross

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,25 @@
 <template>
-  <section id="header" />
+  <section id="header"/>
   <nav :class="navClasses" style="margin-top: -40px;" id="header">
     <div class=" flex flex-wrap items-center justify-between mx-auto px-8 py-3 sm:px-0 sm:p-6 sm:w-5/6">
       <a href="https://flowbite.com/" class="flex items-center ">
         <span class="self-center sm:text-2xl  whitespace-nowrap text-[#D6FF01] font-bold">VISIONARY PARTNERS</span>
       </a>
-      <button data-collapse-toggle="navbar-default" type="button"
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-Z rounded-lg md:hidden text-white focus:text-[#D6FF01]"
-              aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-      </button>
+
+      <div data-collapse-toggle="navbar-default"
+           class="inline-flex items-center px-1 w-10 h-10 justify-center text-sm text-Z rounded-lg md:hidden text-white focus:text-[#D6FF01]"
+           aria-controls="navbar-default" aria-expanded="false" id="app">
+        <div class="menu-wrapper" @click="openMenu = !openMenu" :class="{ active: openMenu }">
+          <div class="menu-bar one"></div>
+          <div class="menu-bar two"></div>
+          <div class="menu-bar three"></div>
+        </div>
+      </div>
 
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul class="flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 ">
           <li>
-            <router-link data-toggle="navbar-default" to="#waarom"
+            <router-link to="#waarom"
                          class="mt-3 md:mt-0 block py-2 pl-3 pr-4 text-white hover:text-[#D6FF01] md:p-0 cursor-pointer">
               Wie zijn wij
             </router-link>
@@ -49,11 +50,14 @@
 </template>
 
 <script>
+
+
 export default {
   name: "Header",
   data() {
     return {
       isTop: true, // Initial state, assuming the page is loaded at the top
+      openMenu: false,
     };
   },
   computed: {
@@ -82,5 +86,98 @@ export default {
 </script>
 
 <style scoped>
-/* Your styles here */
+
+.menu-wrapper {
+  width: 40px;
+  height: auto;
+  cursor: pointer;
+  position: relative;
+}
+
+.menu-bar {
+  position: absolute;
+  width: 100%;
+  height: 4px;
+  background: #fff;
+  left: 0%;
+}
+
+.menu-wrapper:hover .menu-bar {
+  /*animation-duration: 1s;
+  animation-name: slideOut;
+  animation-direction: alternate;
+  animation-timing-function: ease-out;*/
+}
+
+
+.one {
+  top: -8px;
+  animation-delay: 0.1s;
+  transition: all 0.3s;
+}
+
+.two {
+  top: 0;
+  transition: all 0.3s;
+}
+
+.three {
+  top: 8px;
+  transition: all 0.3s;
+}
+
+
+@keyframes slideOut {
+  0% {
+    width: 100%;
+    left: 0%;
+    right: auto;
+  }
+
+  50% {
+    width: 0%;
+    left: 0%;
+    right: auto;
+  }
+  51% {
+    width: 0%;
+    right: 0%;
+    left: auto;
+  }
+
+  100% {
+    width: 100%;
+    right: 0%;
+    left: auto;
+  }
+}
+
+
+/**/
+
+.menu-wrapper:hover .menu-bar.active {
+  animation: none;
+}
+
+.active .one {
+  top: 50%;
+  left: 0%;
+  transform: rotate(45deg);
+  background: #D6FF01;
+}
+
+.active .two {
+  top: 50%;
+  left: 0%;
+  transform: rotate(-45deg);
+  background: #D6FF01;
+}
+
+.active .three {
+  top: 50%;
+  left: 0%;
+  transform: rotate(-45deg);
+  background: #D6FF01;
+}
 </style>
+
