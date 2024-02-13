5986586900e6ba941698a1464c9f514c9d90db47
Ilias <ilias@aravis.nl>
2024-02-13 12:36:38 +0100

    removed old code

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -17,12 +17,6 @@
             <span v-typed-js></span>
         </VueTyped>
       </h2>
-      <h1>
-        Hi, I'm a
-        <span class="typed-text">{{ typeValue }}</span>
-        <span class="blinking-cursor">|</span>
-        <span class="cursor" :class="{ typing: typeStatus }">&nbsp;</span>
-      </h1>
       <p class="text-lg sm:text-xl text-left mt-8">Met ons hoog opgeleid team staat uw website of app binnen 2 weken online. Ons
         team bestaat
         uit HBO-programmeurs met minstens 8 jaar aan ervaring en kennis in hun vak gebied!</p>
@@ -41,131 +35,19 @@
 <script>
 export default {
   name: "Hero",
-  data: () => {
-    return {
-      typeValue: "",
-      typeStatus: false,
-      displayTextArray: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-      typingSpeed: 100,
-      erasingSpeed: 100,
-      newTextDelay: 2000,
-      displayTextArrayIndex: 0,
-      charIndex: 0,
-    };
-  },
-  props: {},
-  created() {
-    setTimeout(this.typeText, this.newTextDelay + 200);
-  },
-  methods: {
-    typeText() {
-      if (this.charIndex < this.displayTextArray[this.displayTextArrayIndex].length) {
-        if (!this.typeStatus) this.typeStatus = true;
-        this.typeValue += this.displayTextArray[this.displayTextArrayIndex].charAt(
-            this.charIndex
-        );
-        this.charIndex += 1;
-        setTimeout(this.typeText, this.typingSpeed);
-      } else {
-        this.typeStatus = false;
-        setTimeout(this.eraseText, this.newTextDelay);
-      }
-    },
-    eraseText() {
-      if (this.charIndex > 0) {
-        if (!this.typeStatus) this.typeStatus = true;
-        this.typeValue = this.displayTextArray[this.displayTextArrayIndex].substring(
-            0,
-            this.charIndex - 1
-        );
-        this.charIndex -= 1;
-        setTimeout(this.eraseText, this.erasingSpeed);
-      } else {
-        this.typeStatus = false;
-        this.displayTextArrayIndex += 1;
-        if (this.displayTextArrayIndex >= this.displayTextArray.length)
-          this.displayTextArrayIndex = 0;
-        setTimeout(this.typeText, this.typingSpeed + 1000);
-      }
-    },
-  },
 }
 </script>
 
 <style lang="scss" scoped>
 
-/*!* Style for the typing cursor *!
+/* Style for the typing cursor */
 .vue-typed-js .typed-cursor {
   opacity: 1;
   animation: blink 0.7s infinite;
-  color: blue; !* Cursor color *!
+  color: blue; /* Cursor color */
 }
 
 @keyframes blink {
   50% { opacity: 0; }
-}*/
-
-h1 {
-  font-size: 6rem;
-  font-weight: normal;
-  span.typed-text {
-    color: #d2b94b;
-  }
-}
-
-// Cursor blinking CSS Starts...
-.blinking-cursor {
-  font-size: 6rem;
-  color: #2c3e50;
-  -webkit-animation: 1s blink step-end infinite;
-  -moz-animation: 1s blink step-end infinite;
-  -ms-animation: 1s blink step-end infinite;
-  -o-animation: 1s blink step-end infinite;
-  animation: 1s blink step-end infinite;
-}
-@keyframes blink {
-  from,
-  to {
-    color: transparent;
-  }
-  50% {
-    color: #2c3e50;
-  }
-}
-@-moz-keyframes blink {
-  from,
-  to {
-    color: transparent;
-  }
-  50% {
-    color: #2c3e50;
-  }
-}
-@-webkit-keyframes blink {
-  from,
-  to {
-    color: transparent;
-  }
-  50% {
-    color: #2c3e50;
-  }
-}
-@-ms-keyframes blink {
-  from,
-  to {
-    color: transparent;
-  }
-  50% {
-    color: #2c3e50;
-  }
-}
-@-o-keyframes blink {
-  from,
-  to {
-    color: transparent;
-  }
-  50% {
-    color: #2c3e50;
-  }
 }
 </style>
